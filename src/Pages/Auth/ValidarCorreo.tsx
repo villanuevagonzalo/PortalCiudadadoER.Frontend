9fc22f29cfacc1438e11758f7b51096c1fce9a94
Gonzalo Villanueva <gonzalovillanueva.06@gmail.com>
2023-01-11 07:11:24 -0300

    .

--- src/Pages/Auth/ValidarCorreo.tsx
+++ src/Pages/Auth/ValidarCorreo.tsx
@@ -1,6 +1,6 @@
 import { useContext, useRef, useState } from 'react';
 import { formGetInitialValues, formGetValidations, FormStateDefault, FormStateProps } from "../../Interfaces/FormFields";
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthContext';
 
 import { LabelDiv, MainContainer, Sidebar, Spinner, SubtitleDiv, TitleDiv, ToDo } from '../../Components/Elements/StyledComponents';
@@ -10,18 +10,23 @@ import { Formik, Form } from 'formik';
 import { FormikField } from '../../Components/Forms/FormikField';
 import { AiOutlineLock } from 'react-icons/ai';
 import { Descripcion } from '../../Components/Elements/Descripcion';
+import { GetParams } from '../../Utils/generalFunctions';
 
 
 
 export const ValidarCorreo = () =>{
 
-    const ref:any = useRef(null);
-    const { Login } = useContext(AuthContext);
+/*    const [searchParams] = useSearchParams();
+    const code: string | null = searchParams.get("code")
+    const cuil: string | null = searchParams.get("cuil")
+*/
+    console.log(GetParams(["code","cuil"]))
 
     return(<>
         <Sidebar open={true}>
             <LogoCiudadanoDigital/>
             <br />
+            {/*(code && cuil)?"Si":"NO"*/}
             <TitleDiv>Validar Correo<AiOutlineLock/></TitleDiv>
             <SubtitleDiv>Ingresá tus datos para iniciar sesión en la plataforma.</SubtitleDiv>
             
--- src/Utils/generalFunctions.ts
+++ src/Utils/generalFunctions.ts
@@ -1,14 +1,66 @@
 // Genral Functions
 
-export const CapitalizeWords = (sentence:string) =>
-    sentence
-        .split(" ")
-        .map((word:string) =>
-            word[0].toUpperCase() + word.substring(1).toLowerCase()
-        )
-        .join(" ")
+import { useSearchParams } from 'react-router-dom'
+
+export const CapitalizeWords = (sentence: string) =>
+  sentence
+    .split(' ')
+    .map(
+      (word: string) => word[0].toUpperCase() + word.substring(1).toLowerCase(),
+    )
+    .join(' ')
+
+export const Sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
 
 
-export const Sleep = (ms: number) => 
-    new Promise(resolve => setTimeout(resolve, ms));
+interface GetParamsProps {
+  value: string;
+  error: string;
+}
 
+export const GetParams = (params: string[]) => {
+    const [searchParams] = useSearchParams()
+    const newParams: {[key: string]: { value: string; error: string; }} = {}
+  
+    for (let param in params) {
+        const paramname = params[param]
+      const paramvalue = searchParams.get(params[param]) as string
+      if (paramvalue) {
+        try {
+          newParams[paramname] = {value:atob(paramvalue),error:''}
+        } catch (e) {
+            
+          newParams[paramname] = {value:'',error:`El campo <b>${paramname}</b> presenta un valor invalido`}
+        }
+      } else {
+        newParams[paramname] = {value:'',error:`El campo <b>${paramname}</b> no existe`}
+    }
+    }
+  
+    return newParams
+  }
+
+  
+  
+export const GetParams2 = (params: string[]) => {
+    const [searchParams] = useSearchParams()
+    const newParams: string[] = []
+  
+    for (let param in params) {
+      console.log(params[param])
+      const tempparam = searchParams.get(params[param]) as string
+      if (tempparam) {
+        try {
+          newParams.push(atob(tempparam))
+        } catch (e) {
+          newParams.push('string invalido')
+        }
+      } else {
+        newParams.push('')
+      }
+    }
+  
+    return newParams
+  }
+  
