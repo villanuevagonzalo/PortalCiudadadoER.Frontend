dc55b007e9bf72c3ac5d19cd1ac59018855fb1ae
Sergio Comas <comassergio1@gmail.com>
2023-01-11 20:32:26 -0300

    Error page format modified

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,25 +1,34 @@
-import { BiError, BiArrowToLeft } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import { Hero } from "../Components/Elements/Hero";
+import { FormikError, LabelDiv, MainContainer, NavigatorSpacer, NavigatorWrapper, Sidebar, SubtitleDiv, Title2Div, TitleDiv } from "../Components/Elements/StyledComponents";
+import { LogoCiudadanoDigital } from "../Components/Images/LogoCiudadanoDigital";
+import { Button } from "../Components/Forms/Button";
+import { AiOutlineLeft } from 'react-icons/ai'
+import { Descripcion } from "../Components/Elements/Descripcion";
 
 export const ErrorPage = () => {
-  return (
-    <Hero classes="bg-gradient-to-r from-rose-500 to-orange-500 text-white" tail={true}>
-    <div className="p-4">
-      <h1 className="my-4 text-4xl lg:text-5xl font-bold leading-tight flex space-x-4 justify-center w-full md:justify-start">
-        <BiError aria-hidden="true" className="h-10 lg:h-14 w-auto mr-4"/>
-        ERROR
-      </h1>
-      <p className="leading-normal text-1xl mb-4 lg:text-2xl justify-center w-full md:justify-start">
-        La pagina solicitada no existe.
-      </p>
-      <div className="flex space-x-4 justify-center w-full md:justify-start mb-8">
-        <Link to="/" className="bg-white text-gray-800 flex font-bold rounded-lg my-6 py-4 px-6 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-110 duration-300 ease-in-out">
-         
-        <BiArrowToLeft aria-hidden="true" className="h-6 w-auto mr-4"/> Volver al Inicio
+  return(<>
+    <Sidebar open={true}>
+      <LogoCiudadanoDigital/>
+        <br />
+        <br />
+        <div className="flex gap-4 justify-center">
+        </div>
+        <div className="flex gap-4 justify-center">
+        <TitleDiv>¡Error!</TitleDiv>
+        </div>
+        <div className="flex gap-4 justify-center">
+        <TitleDiv>La pagina solicitada no existe</TitleDiv>
+        </div>
+        <br />
+        <Link to="/" className="w-full">
+        <Button color="gray" className="w-full">
+        <AiOutlineLeft aria-hidden="true" className="h-6 w-auto mr-2"/> 
+            Volver al Inicio                            
+        </Button>
         </Link>
-      </div>
-      </div>
-    </Hero>
-  );
+    </Sidebar>
+    <MainContainer>
+      <Descripcion />
+    </MainContainer>
+  </>)
 };
