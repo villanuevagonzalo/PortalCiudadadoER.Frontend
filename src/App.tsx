a62420e10aea8fb4adbb5cce848f28d1d8f930d1
Sergio Comas <comassergio1@gmail.com>
2023-01-16 19:09:57 -0300

    update Dashboard Ciudadano page

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route index element={<HomeRoute />} />
-            <Route path="Inicio" element={<HomePage />} />
+            <Route path="Inicio" element={<DashboardCiudadanoPage />} />
             <Route path="Ingresar" element={<LoginPage />} />
             <Route path="Registro" element={<RegisterPage />} />
             <Route path="ValidarCorreo" element={<ValidarCorreo />} />
