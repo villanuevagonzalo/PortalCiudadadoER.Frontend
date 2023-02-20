b1518e2dbbd6af968fafe1c4db3d120e98855a9b
Sergio Comas <comassergio1@gmail.com>
2023-02-20 11:03:35 -0300

    Create Dashboard Actor Layout ad pages

---
+++ src/Pages/DashboardActor/Config.tsx
@@ -0,0 +1,29 @@
+
+import { LayoutItem, LayoutTitle } from "../../Components/Elements/StyledComponents"
+import { RawLocations, GetLocations } from "../../Utils/LocationsFunctions"
+
+export const DashboardActor_ConfigurationPage = () => {
+
+
+
+    RawLocations().then((response)=>{
+        console.log(GetLocations(response.data))
+    }).catch((e:any)=>{
+        
+    })
+
+  return (<>
+    <LayoutTitle>
+      Mi Perfil
+    </LayoutTitle>
+    <LayoutItem className="flex items-center gap-1 p-10">
+        COMPLETAR
+    </LayoutItem>
+    <LayoutTitle>
+      Nivel de Usuario
+    </LayoutTitle>
+    <LayoutItem className="flex items-center gap-1 p-10">
+        COMPLETAR
+    </LayoutItem>
+  </>)
+}
