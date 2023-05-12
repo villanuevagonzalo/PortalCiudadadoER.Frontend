4bac861af1bb1c34a5d215808cf301f0315bc647
Sergio Comas <comassergio1@gmail.com>
2023-05-12 19:36:40 -0300

    test image upload

---
+++ src/Components/Forms/FromikImage.tsx
@@ -0,0 +1,59 @@
+import { ErrorMessage, Field, useField, useFormikContext } from "formik";
+import { FormError, FormWrapper, FormWrapperInput } from "./StyledComponents";
+import { useState } from "react";
+
+
+interface Props{
+    name: string;
+    autoFocus?: boolean;
+    hidden?: boolean;
+    disabled?: boolean;
+    component?: React.Component;
+    label?: string;
+    className?: string;
+}
+
+export const FormikImage = ({...props}: Props) => {
+
+    const[Image, setImage] = useState<string | null>(null);
+
+    const [ field ] = useField(props.name)
+
+    const { setFieldValue } = useFormikContext();
+
+    function onChange(event:any) {
+        try{
+            const file = event.currentTarget.files[0];
+            setFieldValue(field.name,file)
+        } catch(e){
+            console.log(e)
+        }
+        
+        // console.log("Image file:", file);
+        // const reader = new FileReader();
+        // reader.onload = () => {
+        //   setImage(reader.result as string);
+        // };
+        // reader.readAsDataURL(file);
+        
+        // console.log("Image value:", reader.result);
+      }
+
+
+      return (
+        <FormWrapper className={props.className}>
+            <FormWrapperInput error={false} disabled={props.disabled} focus={true}>
+                <div>
+                    <label htmlFor={props.name}>{props.label}</label>
+                    <input type="file" autoFocus={props.autoFocus} {...field} {...props} onChange={onChange}/>
+                    {/* {image && (
+                        <img src={`data:image/jpeg;base64,${image}`} alt="preview" style={{ maxWidth: "100%" }} />
+                        )
+                    } */}
+                </div>
+            </FormWrapperInput>
+            <ErrorMessage name={props.name} component={FormError}/>
+        </FormWrapper>
+      )
+
+}
