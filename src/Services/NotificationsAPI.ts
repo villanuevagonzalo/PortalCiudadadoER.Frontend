c4ddb013d7487fbffc129708e285ed5c35926b3d
Gonzalo Villanueva <gonzalovillanueva.06@gmail.com>
2023-05-12 21:13:18 -0300

    Hotfix

--- src/Services/NotificationsAPI.ts
+++ src/Services/NotificationsAPI.ts
@@ -17,12 +17,16 @@ export class NotificationsAPI {
     message_title: string;
     message_body: string;
     attachment_type: string;
-    attachment: string;
+    attachment: File;
     notification_date_from: Date;
     notification_date_to: Date;
     send_by_email: string;
   }){
-    return this.baseService.post('/v0/notification/new', params)
+    return this.baseService.post('/v0/notification/new', params,{
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
   }
 
   public Get(params: {
@@ -30,4 +34,12 @@ export class NotificationsAPI {
     return this.baseService.get('/v0/notification/get/user/news', { params })
   }
 
+  public GetAttachment(params: {
+    message_title: string;
+  }){
+    return this.baseService.get('/v0/notification/get/user/attachments', { params })
+  }
+
+
+  
 }
