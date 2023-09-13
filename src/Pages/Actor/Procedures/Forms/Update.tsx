517249ddc8cb5f782537f9021df604ed9425e4e7
julianov <julianov403@gmail.com>
2023-09-12 23:18:48 -0300

    add arrow down and arrow up in form creator and form update

--- src/Pages/Actor/Procedures/Forms/Update.tsx
+++ src/Pages/Actor/Procedures/Forms/Update.tsx
@@ -18,7 +18,7 @@ import { DefaultFormState } from "../../../../Data/DefaultValues";
 import { FormContext } from "../../../../Contexts/FormContext";
 import { Spinner } from "../../../../Components/Elements/StyledComponents";
 import { BackOfficesFormElement } from "../../../../Modules/Actor/FormsElement";
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { HiArrowDown, HiArrowUp, HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { FormikButton } from "../../../../Components/Forms/FormikButton";
 import { Link } from "react-router-dom";
 import { Pages } from "../../../../Routes/Pages";
@@ -105,6 +105,31 @@ export const FormUpdate: React.FC<Arguments> = ({formToUpdate, setVolver}) => {
     setErrorCarga(true)
    }
  }
+
+ const moveElementUp = (index:number) => {
+  if (index > 0) {
+    // Swap the element with the one above it
+    const updatedFields = [...fields];
+    const temp = updatedFields[index];
+    updatedFields[index] = updatedFields[index - 1];
+    updatedFields[index - 1] = temp;
+    // Update the state with the new order
+    setFields(updatedFields);
+  }
+};
+
+const moveElementDown = (index:number) => {
+  if (index < fields.length - 1) {
+    // Swap the element with the one below it
+    const updatedFields = [...fields];
+    const temp = updatedFields[index];
+    updatedFields[index] = updatedFields[index + 1];
+    updatedFields[index + 1] = temp;
+    // Update the state with the new order
+    setFields(updatedFields);
+  }
+};
+
   
  if (ver){
 
@@ -171,28 +196,42 @@ export const FormUpdate: React.FC<Arguments> = ({formToUpdate, setVolver}) => {
           >
             <Form autoComplete="off">
             {fields.map((element: ElementInstance<ElementSchemaTypes>, index: number) => (
-              <div key={element.name}  style={{display:"flex", flexDirection:"column", width:"auto", margin:"10px 0px 15px 0px"}}>
-                <p style={{margin:"0px 0px 10px 0px"}}>{element.type}</p>
-              
-                <Element instance={element} />
-                <div style={{display:"flex", flexDirection:"row", width:"auto", margin:"5px 0px 15px 0px"}}> 
-                
-                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                  <div onClick={() => editarComponente(element, index)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
-                    </svg>
-                  </div>
-                  <div onClick={() => borrarComponente(index)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                  </div>            
-                </div>
-    
-                </div>
-              </div>
-            ))}  
+                      <div key={element.name} style={{ display: "flex", flexDirection: "column", width: "auto", margin: "10px 0px 15px 0px" }}>
+                        <p style={{ margin: "0px 0px 10px 0px" }}>{element.type}</p>
+
+                        <Element instance={element} />
+                        <div style={{ display: "flex", flexDirection: "row", width: "auto", margin: "5px 0px 15px 0px" }}>
+                          <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                            <div style={{display:"flex", flexDirection:"row"}} >
+
+                              <div onClick={() => editarComponente(element, index)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                                </svg>
+                              </div>
+                              {index !== 0 && ( // Only render "Move Up" if not the first element
+                              <div style={{ margin: "0px 0px 0px 15px" }}>
+                                {/* Move Up Button */}
+                                <button onClick={() => moveElementUp(index)}><HiArrowUp /></button>
+                              </div>
+                            )}
+                            {index !== fields.length - 1 && ( // Only render "Move Down" if not the last element
+                              <div style={{ margin: "0px 0px 0px 15px" }}>
+                                {/* Move Down Button */}
+                                <button onClick={() => moveElementDown(index)}><HiArrowDown /></button>
+                              </div>
+                            )}
+                            </div>
+                            <div onClick={() => borrarComponente(index)}>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                              </svg>
+                            </div>
+                          </div>
+                         
+                        </div>
+                      </div>
+                    ))}
               <LayoutStackedPanel>
                 <LayoutSpacer/>
                 <div style={{display:"flex", flexDirection:"row", width:"auto", margin:"10px 0px 15px 0px"}}> 
