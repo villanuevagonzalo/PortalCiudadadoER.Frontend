799d9400f2bc143a3d19102e07eee457373466f7
julianov <julianov403@gmail.com>
2023-11-27 15:52:21 -0300

    hot fix personal data form

--- src/Interfaces/FormFields.ts
+++ src/Interfaces/FormFields.ts
@@ -168,21 +168,7 @@ export const FormFields:FieldProps = {
         placeholder: 'Ingresa tu fecha de nacimiento',
         validations: yup.date()
           .required('El campo es obligatorio')
-          .test('validar-fecha', 'Ingresa un año válido', function (value) {
-            // Asegurar que el año tenga cuatro dígitos y no comience con ceros
-            const year = value.getFullYear();
-            console.log("the year is: " + year);
-      
-            const currentYear = new Date().getFullYear();
-            const twoDigitYearThreshold = 50; // Ajusta este umbral según tus necesidades
-      
-            if (year < currentYear - 10 || year > currentYear || (year < currentYear - 2000 && year < twoDigitYearThreshold)) {
-              throw this.createError({ message: 'Ingresa un año válido' });
-            }
-      
-            return true;
-          })
-          .min(new Date(new Date().getFullYear() - 120, new Date().getMonth(), new Date().getDate()), 'La fecha de nacimiento no puede tener más de 120 años')
+          .min(new Date(new Date().getFullYear() - 100, new Date().getMonth(), new Date().getDate()), 'La fecha de nacimiento no puede tener más de 100 años')
           .max(new Date(new Date().getFullYear() - 1, new Date().getMonth(), new Date().getDate()), 'La fecha de nacimiento debe tener por lo menos 1 año.'),
       },
 
